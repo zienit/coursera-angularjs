0d6c0ec65dd13c4d556be844b2e522c1a9eceb67
bezemerh <hbz@klaverblad.nl>
2016-10-20 23:13:22 +0200

    sign up form

---
+++ module5-solution/src/public/public.routes.js
@@ -0,0 +1,65 @@
+(function() {
+'use strict';
+
+angular.module('public')
+.config(routeConfig)
+.run(['$rootScope',function($rootScope) {
+  $rootScope.$on('$stateChangeError',
+    function(event, toState, toParams, fromState, fromParams, error){
+      console.log(event);
+      console.log(error);
+    });
+}]);
+
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig ($stateProvider) {
+  // Routes
+  $stateProvider
+    .state('public', {
+      absract: true,
+      templateUrl: 'src/public/public.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url: '/menu/{category}',
+      templateUrl: 'src/public/menu-items/menu-items.html',
+      controller: 'MenuItemsController',
+      controllerAs: 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/home/signup.html'/*,
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }*/
+    })
+    ;
+}
+
+})();
